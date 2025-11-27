37ae25ed4023c80dc6be2931dc318a59ecde25c4
yuumuu <haidar.alhazen93@gmail.com>
2025-11-27 15:28:48 +0700

    feat: introduce users data, add navigation bar, create home and users pages, and update routing.

--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ const routes = [
     { path: '/about', component: 'app/pages/about.html' },
     { path: '/divisions', component: 'app/pages/divisions/index.html' },
     { path: '/division/:slug', component: 'app/pages/divisions/detail.html' },
+    { path: '/users', component: 'app/pages/users/index.html' },
     { path: '/404', component: 'app/pages/404.html' }
 ];
